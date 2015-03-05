b3eceda650ea32aa126972f3c4f5e49d97f072eb
Jay Hayes <ur@iamvery.com>
2015-03-05 15:53:50 -0600

    Configure cross origin settings

--- lib/blastermind/app.rb
+++ lib/blastermind/app.rb
@@ -9,6 +9,12 @@ module Blastermind
     register Sinatra::CrossOrigin
     enable :cross_origin
 
+    set :allow_origin, :any
+    set :allow_methods, [:get, :post, :options]
+    set :allow_credentials, true
+    set :max_age, "1728000"
+    set :expose_headers, ['Content-Type']
+
     use Rack::Deflater
     use Routes::Matches
 
