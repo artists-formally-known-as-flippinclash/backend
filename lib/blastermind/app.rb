a64a1647a8a0dcaeee17d222dc0b4ed08d957bcd
Jay Hayes <ur@iamvery.com>
2015-03-05 15:34:59 -0600

    Enable cross origin requests

--- lib/blastermind/app.rb
+++ lib/blastermind/app.rb
@@ -1,10 +1,14 @@
 require "sinatra"
+require "sinatra/cross_origin"
 require "blastermind/db"
 require "blastermind/pusher"
 require "blastermind/routes/matches"
 
 module Blastermind
   class App < Sinatra::Application
+    register Sinatra::CrossOrigin
+    enable :cross_origin
+
     use Rack::Deflater
     use Routes::Matches
 
