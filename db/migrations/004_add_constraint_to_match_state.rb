3957dad516a8a18c754f0f959ca6032b83c60ea7
Jay Hayes <ur@iamvery.com>
2015-03-05 13:10:26 -0600

    Make migration reversible, safe for null records

--- db/migrations/004_add_constraint_to_match_state.rb
+++ db/migrations/004_add_constraint_to_match_state.rb
@@ -1,7 +1,14 @@
 Sequel.migration do
-  change do
+  up do
+    self[:matches].update(state: "match-making")
     alter_table(:matches) do
       set_column_not_null :state
+    end
+  end
+
+  down do
+    alter_table(:matches) do
+      set_column_allow_null :state
     end
   end
 end
