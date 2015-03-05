4b9111595f58dd992f2f2db46a4d72abe50d8f1e
Jay Hayes <ur@iamvery.com>
2015-03-05 11:26:26 -0600

    Add constants for valid match states

--- lib/blastermind/models/match.rb
+++ lib/blastermind/models/match.rb
@@ -1,6 +1,12 @@
 module Blastermind
   module Models
     class Match < Sequel::Model
+      STATES = [
+        MATCH_MAKING = "match-making".freeze,
+        IN_PROGRESS = "in-progress".freeze,
+        FINISHED = "finished".freeze,
+      ].freeze
+
       def channel
         "match-#{id}"
       end
