dd601b2db241df35f88744350911298d63315f5d
Jay Hayes <ur@iamvery.com>
2015-03-05 16:19:33 -0600

    Revert "Enable cross origin requests"
    This reverts commit a64a1647a8a0dcaeee17d222dc0b4ed08d957bcd.

--- lib/blastermind/app.rb
+++ lib/blastermind/app.rb
@@ -1,14 +1,10 @@
 require "sinatra"
-require "sinatra/cross_origin"
 require "blastermind/db"
 require "blastermind/pusher"
 require "blastermind/routes/matches"
 
 module Blastermind
   class App < Sinatra::Application
-    register Sinatra::CrossOrigin
-    enable :cross_origin
-
     use Rack::Deflater
     use Routes::Matches
 
