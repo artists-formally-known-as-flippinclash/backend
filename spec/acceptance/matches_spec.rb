deb1ed33d96e1484faed4a4916a6adff395c6cf5
Jay Hayes <ur@iamvery.com>
2015-03-05 11:02:54 -0600

    Extract json_response helper

--- spec/acceptance/matches_spec.rb
+++ spec/acceptance/matches_spec.rb
@@ -3,18 +3,18 @@ require "json"
 require "blastermind/models/match"
 
 describe "/matches" do
-  describe "index" do
+  let(:json_response) { JSON.parse(last_response.body) }
+
+  describe "GET index" do
     it "responds with matches data" do
       id = Blastermind::Models::Match.insert(state: "match-making")
       match = Blastermind::Models::Match[id]
 
       get "/matches"
 
-      json = JSON.parse(last_response.body)
-
-      expect(json.fetch("data").first.fetch("id")).to eq(match.id)
-      expect(json.fetch("data").first.fetch("state")).to eq(match.state)
-      expect(json.fetch("data").first.fetch("channel")).to eq(match.channel)
+      expect(json_response.fetch("data").first.fetch("id")).to eq(match.id)
+      expect(json_response.fetch("data").first.fetch("state")).to eq(match.state)
+      expect(json_response.fetch("data").first.fetch("channel")).to eq(match.channel)
     end
   end
 end
