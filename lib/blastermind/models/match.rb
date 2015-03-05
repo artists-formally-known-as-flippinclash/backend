8bed299994c893bf53193e0b484eac26fb834a01
Jay Hayes <ur@iamvery.com>
2015-03-05 12:01:28 -0600

    Add constants for match events

--- lib/blastermind/models/match.rb
+++ lib/blastermind/models/match.rb
@@ -1,6 +1,12 @@
 module Blastermind
   module Models
     class Match < Sequel::Model
+      EVENTS = [
+        MATCH_STARTED = "match-started".freeze,
+        MATCH_PROGRESS = "match-progress".freeze,
+        MATCH_ENDED = "match-ended".freeze,
+      ].freeze
+
       STATES = [
         MATCH_MAKING = "match-making".freeze,
         IN_PROGRESS = "in-progress".freeze,
