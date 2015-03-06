0459113361c3abf3ee72cf7a18c8525c1821a77f
Jay Hayes <ur@iamvery.com>
2015-03-06 14:09:29 -0600

    Implement correct guess case

--- lib/blastermind/routes/guesses.rb
+++ lib/blastermind/routes/guesses.rb
@@ -16,6 +16,25 @@ module Blastermind
       end
 
       post "/matches/:match_id/players/:player_id/guesses" do
+        body = JSON.parse(request.body.read)
+        guess_body = body.fetch("guess")
+        code_pegs = guess_body.fetch("code_pegs")
+        match_id = params.fetch("match_id")
+        player_id = params.fetch("player_id")
+        match = Models::Match[match_id]
+        round = match.current_round
+
+        guess = Models::Guess.create(
+          code_pegs: Sequel.pg_array(code_pegs, :code_pegs),
+          player_id: player_id,
+          round: round,
+        )
+
+        content_type :json
+        status 201
+        guess
+          .extend(Representers::IndividualGuess)
+          .to_json
       end
     end
   end
--- spec/acceptance/guesses_spec.rb
+++ spec/acceptance/guesses_spec.rb
@@ -8,7 +8,20 @@ describe "/matches/{match_id}/players/{player_id}/guesses" do
 
   describe "POST create" do
     context "correct guess" do
-      it "responds with 'correct' outcome"
+      let(:match) { Blastermind::Models::Match.create_to_play }
+      let(:round) { match.current_round }
+      let(:player) { Blastermind::Models::Player.create(name: "J", match: match) }
+
+      it "responds with 'correct' outcome" do
+        guess_params = {
+          guess: { code_pegs: round.solution }
+        }
+
+        post "/matches/#{match.id}/players/#{player.id}/guesses", guess_params.to_json, "CONTENT_TYPE" => "application/json"
+
+        expect(last_response.status).to be(201)
+        expect(response_data.fetch("outcome")).to eq(Blastermind::Models::Guess::CORRECT.to_s)
+      end
     end
 
     context "incorrect guess" do
