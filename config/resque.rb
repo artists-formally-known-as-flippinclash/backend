7d1bef6100c2420bf713ece182c639e3b4ee08d1
Jay Hayes <ur@iamvery.com>
2015-03-06 11:11:11 -0600

    Resque db management around forking

--- config/resque.rb
+++ config/resque.rb
@@ -1,2 +1,5 @@
 require "resque"
+require "sequel/model"
 Resque.redis = ENV.fetch("REDISTOGO_URL")
+Resque.before_fork = Proc.new { Sequel::Model.db.disconnect }
+Resque.after_fork = Proc.new { Sequel::Model.db.connect(ENV.fetch("DATABASE_URL")) }
