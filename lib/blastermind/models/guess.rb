cc8582c83a84782d36fb96d3cdac50d25b365221
Jay Hayes <ur@iamvery.com>
2015-03-06 14:02:28 -0600

    Guess has an outcome based on the round

--- lib/blastermind/models/guess.rb
+++ lib/blastermind/models/guess.rb
@@ -1,8 +1,17 @@
 module Blastermind
   module Models
     class Guess < Sequel::Model
+      OUTCOMES = [
+        CORRECT = :correct,
+        INCORRECT = :incorrect,
+      ].freeze
+
       many_to_one :player
       many_to_one :round
+
+      def outcome
+        correct? ? CORRECT : INCORRECT
+      end
     end
   end
 end
