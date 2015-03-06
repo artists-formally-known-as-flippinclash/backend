c221cd88990486a4f31171d36511231feb8bd684
Jay Hayes <ur@iamvery.com>
2015-03-06 12:30:41 -0600

    Implement hack to read solution from round as array :sob:

--- lib/blastermind/models/round.rb
+++ lib/blastermind/models/round.rb
@@ -4,6 +4,11 @@ require "blastermind/models/code_peg"
 module Blastermind
   module Models
     class Round < Sequel::Model
+      # This is such a hack, it seems I don't know how to use Sequel's pg_array
+      def solution
+        solution_string = super
+        solution_string[1..-2].split(",")
+      end
     end
   end
 end
