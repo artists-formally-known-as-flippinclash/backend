d2478b6904481d307368e25153bfea95b97a06df
Jay Hayes <ur@iamvery.com>
2015-03-06 23:11:52 -0600

    Trigger round even pusher event

--- lib/blastermind/models/round.rb
+++ lib/blastermind/models/round.rb
@@ -1,9 +1,14 @@
 require "sequel/model"
 require "blastermind/models/code_peg"
+require "blastermind/representers/individual_round"
 
 module Blastermind
   module Models
     class Round < Sequel::Model
+      EVENTS = [
+        ENDED = "round-ended".freeze,
+      ].freeze
+
       def self.generate(match, solution = CodePeg.generate_solution)
         new(solution: Sequel.pg_array(solution, :code_peg), match: match)
       end
@@ -19,10 +24,21 @@ module Blastermind
 
       def finished!
         update(finished: true)
+        trigger(ENDED)
       end
 
       def attempt(guess)
         finished! if guess.correct?
+      end
+
+      private
+
+      def trigger(event)
+        # It seems ROAR representers are single-use. I tried to extend the
+        # original instance and reuse it hear, but to_json didn't behave
+        # as expected after to_hash was called ಠ_ಠ
+        pusher_data = extend(Representers::IndividualRound)
+        Pusher[match.channel].trigger(event, pusher_data.to_hash)
       end
     end
   end
